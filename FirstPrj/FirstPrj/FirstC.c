ec71141f88c6c5b08c6f4718f3ea74b8095f40f6
hupan <hupan008@qq.com>
2019-10-08 16:16:01 +0800

    简单的计算正方形面积

--- FirstPrj/FirstPrj/FirstC.c
+++ FirstPrj/FirstPrj/FirstC.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-	// 重新开始系统的学习C吧！！
-	printf("Hello C!!!");
-	getchar();
+	// 20191008 S1
+	//// 重新开始系统的学习C吧！！
+	//printf("Hello C!!!");
+	//getchar();
+
+	// 20191008 S2
+	// 简单的计算正方形面积
+	while (1)
+	{
+		int num;
+		printf("请输入边长:\n");
+		scanf_s("%d", &num);
+		printf("正方形面积为:%d\n", num*num);
+		getchar();
+	}
+
 	return 0;
 }
