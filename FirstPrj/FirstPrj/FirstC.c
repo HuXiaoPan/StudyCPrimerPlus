9bb115d43196426f6bc824b4ec874ac6a610c61f
hupan <hupan008@qq.com>
2019-10-09 10:24:34 +0800

    打印内存中随机位置的值

--- FirstPrj/FirstPrj/FirstC.c
+++ FirstPrj/FirstPrj/FirstC.c
@@ -8,14 +8,19 @@ int main(void) {
 
 	// 20191008 S2
 	// 简单的计算正方形面积
-	while (1)
-	{
-		int num;
-		printf("请输入边长:\n");
-		scanf_s("%d", &num);
-		printf("正方形面积为:%d\n", num*num);
-		getchar();
-	}
+	//while (1)
+	//{
+	//	int num;
+	//	printf("请输入边长:\n");
+	//	scanf_s("%d", &num);
+	//	printf("正方形面积为:%d\n", num*num);
+	//	getchar();
+	//}
+
+	// 20191009 S1
+	// 尝试打印内存中的任意值
+	printf("这个时固定的值%d,这个就是内存中任意一个值%d\n", 10);
+	getchar();
 
 	return 0;
 }
