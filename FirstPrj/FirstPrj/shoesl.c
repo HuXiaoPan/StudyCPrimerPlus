8ab4537e404d887ab43a50d09260b296fed9d2ff
e <hupan008@qq.com>
2019-10-23 14:03:08 +0800

    sizeof 应用

--- FirstPrj/FirstPrj/shoesl.c
+++ FirstPrj/FirstPrj/shoesl.c
@@ -1,22 +1,22 @@
-/*把鞋码转换为英寸*/
-
-#include <stdio.h>
-
-#define ADJUST 7.31 // 字符常量
-
-int main(void)
-{
-	const double SCALE = 0.333;
-	double shoe, foot;
-	shoe = 3.0;
-	printf("Shoe size (men's)             foot length\n");
-	while (shoe < 18.5)
-	{
-		foot = SCALE * shoe + ADJUST;
-		printf("%10.1f%15.2f inches\n", shoe, foot);
-		shoe++;
-	}
-
-	getchar();
-	return 0;
-}
+///*把鞋码转换为英寸*/
+//
+//#include <stdio.h>
+//
+//#define ADJUST 7.31 // 字符常量
+//
+//int main(void)
+//{
+//	const double SCALE = 0.333;
+//	double shoe, foot;
+//	shoe = 3.0;
+//	printf("Shoe size (men's)             foot length\n");
+//	while (shoe < 18.5)
+//	{
+//		foot = SCALE * shoe + ADJUST;
+//		printf("%10.1f%15.2f inches\n", shoe, foot);
+//		shoe++;
+//	}
+//
+//	getchar();
+//	return 0;
+//}
